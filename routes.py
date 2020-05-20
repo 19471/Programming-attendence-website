0b30a8ec3ae998390127fae281b0888c9a7f9b30
Cameron <19471@burnside.school.nz>
2020-05-20 12:10:08 +1200

    working page with no content created

--- routes.py
+++ routes.py
@@ -1,2 +1,17 @@
 # routes.py for automated attendance 
 
+# import ilbrarys 
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+#route for home page
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+
+if (__name__) == '__main__':
+    app.run(debug=True)
