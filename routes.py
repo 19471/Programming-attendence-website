b31109828e9114f08c32389c423c3aeed9d40d54
cameron <19471@burnside.school.nz>
2020-05-26 08:53:58 +1200

    can add first name and last name and edit first name aswell as delete user

--- routes.py
+++ routes.py
@@ -7,6 +7,10 @@ from flask import request
 from flask import redirect
 import sqlite3
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms import Form, SelectField, SubmitField, TextAreaField, TextField, validators, ValidationError
+from wtforms.validators import DataRequired, Length
 
 #import tables from modules.py
 # from modules.py import * 
@@ -17,13 +21,20 @@ database_file = database_file = "sqlite:///{}".format(os.path.join(project_dir, 
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+app.config["SECRET_KEY"] = "0"
 db = SQLAlchemy(app)
+
+# add flask forms 
+class new_user(FlaskForm):
+    fname = StringField("first name",[validators.length(min=1, max=40), validators.input_required()])
+    lname = StringField("last name ",[validators.length(min=1, max=40), validators.input_required()])
+
 
 # create user table
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     fname = db.Column(db.String(20), nullable=False)
-    lname = db.Column(db.String(20), nullable=False)
+    lname = db.Column(db.String(20), nullable=True)
     student_id = db.Column(db.Integer)
     auth = db.Column(db.String(20))
 
@@ -35,12 +46,13 @@ def __repr__(self):
 #route for home page
 @app.route('/', methods=["GET", "POST"])
 def home():
-    if request.method == "POST":
-        user = User(fname=request.form.get("first name"))
+    form = new_user()
+    if request.form:
+        user = User(fname=form.fname.data, lname=form.lname.data)
         db.session.add(user)
         db.session.commit()
     users = User.query.all()
-    return render_template('home.html', users=users)
+    return render_template('home.html', users=users, form=form)
 
 # update route to update fname in user 
 @app.route("/update", methods=["POST"])
