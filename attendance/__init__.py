babf166ee99e328e98d05925bc72b2d50de1a84c
cameron <19471@burnside.school.nz>
2020-07-21 08:55:01 +1200

    added login manager for flask login

--- attendance/__init__.py
+++ attendance/__init__.py
@@ -1,5 +1,5 @@
-# routes.py for automated attendance 
-# this file contains all of the routes for the attendace project 
+# routes.py for automated attendance
+# this file contains all of the routes for the attendace project
 
 # import ilbrarys
 from datetime import datetime
@@ -7,13 +7,13 @@ import os
 from flask import Flask
 import sqlite3
 from flask_sqlalchemy import SQLAlchemy
-# flask login 
+# flask login
 from flask_login import LoginManager
 from flask_login import UserMixin
-from functools import wraps 
-# crypt 
+from functools import wraps
+# crypt
 from flask_bcrypt import Bcrypt
-# define database 
+# define database
 project_dir = os.path.dirname(os.path.abspath(__file__))
 database_file = database_file = "sqlite:///{}".format(os.path.join(project_dir, "attendance_project.db"))
 
@@ -22,9 +22,12 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 app.config["SECRET_KEY"] = "0"
 db = SQLAlchemy(app)
-login = LoginManager(app)
 bcrypt = Bcrypt(app)
-from attendance.models import * 
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from attendance.models import *
 db.create_all()
 
 
