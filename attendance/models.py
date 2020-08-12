24c3c63aa9369c783ef4ac327fc057c7b06fb463
Cameron <19471@burnside.school.nz>
2020-08-12 12:24:50 +1200

    added posts to user table

--- attendance/models.py
+++ attendance/models.py
@@ -24,7 +24,7 @@ class User(UserMixin ,db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
-    #posts = db.relationship('Post', backref='author', lazy=True)
+    posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
             return f"User('{self.fname}', '{self.lname}', '{self.student_id}', '{self.email}')"
